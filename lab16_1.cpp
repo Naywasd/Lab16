c5a9de39ac3c30bd6bd11d10e84f2f0fb32dc015
Naywasd <sermsub2549@gmail.com>
2026-02-05 23:23:54 +0700

    Update lab16_1.cpp

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,38 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *dPtr, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr + i);
+        if((i + 1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *dPtr, int N, int M) {
+    for (int i = 0; i < N*M; i++) {
+        *(dPtr + i) = (rand() % 101) / 100.0;
+    }
+}
+
+void findRowSum(const double *dPtr, double *sum1, int N, int M) {
+    for (int i = 0; i < N; i++) {
+        double rowTotal = 0;
+        for (int j = 0; j < M; j++) {
+            rowTotal += *(dPtr + (i * M) + j);
+        }
+        sum1[i] = rowTotal;
+    }
+}
+
+void findColSum(const double *dPtr, double *sum2, int N, int M) {
+    for (int j = 0; j < M; j++) {
+        double colTotal = 0;
+        for (int i = 0; i < N; i++) {
+            colTotal += *(dPtr + (i * M) + j);
+        }
+        sum2[j] = colTotal;
+    }
+}
